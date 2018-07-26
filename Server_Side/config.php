71904398fcf61173b6d8b974ed77efbcb8a6ca00
Ponty93 <41543424+Ponty93@users.noreply.github.com>
2018-07-26 13:51:01 +0200

    Update config.php
    //fixed the wrong name of the DB since it has changed

--- Server_Side/config.php
+++ Server_Side/config.php
@@ -3,7 +3,7 @@
 $servername = "psionoff.mysql.db.internal";
 $username = "psionoff_LDS";
 $password = "nOJ0i)O4tzICV@cFe@";
-$dbname = "psionoff_LDS";
+$dbname = "psionoff_LDC";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 ?>
