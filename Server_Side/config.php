ce949a88c53433c6ec961975b283d88d71e76e2d
Ponty93 <41543424+Ponty93@users.noreply.github.com>
2018-07-26 13:42:48 +0200

    Create config.php

---
+++ Server_Side/config.php
@@ -0,0 +1,13 @@
+<?php
+//dati accesso serben per Ludus DS
+$servername = "psionoff.mysql.db.internal";
+$username = "psionoff_LDS";
+$password = "nOJ0i)O4tzICV@cFe@";
+$dbname = "psionoff_LDS";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
+
+
+
+
