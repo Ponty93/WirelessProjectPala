30e07efbc987fd60521deb96745926a17dbf0977
Matteo Lucato <41543424+Ponty93@users.noreply.github.com>
2018-08-06 14:14:30 +0200

    Handler update
    Updated handler with the new structure , everything works as it is at the moment. will end soon

--- Server_Side/handler.php
+++ Server_Side/handler.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 	include('config.php');
 	$content = file_get_contents("php://input"); //get all data sent
 	$update = json_decode($content, true);  //decode dat juicy jsonObject
@@ -12,44 +12,79 @@
 	
 	$pawnId = isset($update['pawnId']) ? $update['pawnId'] : "";
 	$playerId = isset($update['playerId']) ? $update['playerId'] : "";
+	$player2Id = isset($update['player2Id']) ? $update['player2Id'] : "";
 	$op = isset($update['op']) ? $update['op'] : "";
 	$pos = isset($update['pos']) ? $update['pos'] : "";
 	$gameId = isset($update['gameId']) ? $update['gameId'] : "";
 	$valToUpdate = isset($update['valToUpdate']) ? $update['valToUpdate'] : "";
 	
-	
+	//CREATE implementation
+	$tableName = isset($update['tableName']) ? $update['tableName'] : "";
+	$val1 = isset($update['val1']) ? $update['val1'] : "";
+	$val2 = isset($update['val2']) ? $update['val2'] : "";
 
-	if($op == "") // if checking what kind of operation i'm requesting
-	{
-		$sql="" ; 
-		//...
-		$parameters = array('1' => $..., ... );
+	
+	
+	
+	//CREATE IF
+	if($op == "CREATE") // if checking what kind of operation i'm requesting
+	{		
+	
+		if($tableName=="player"){
+				//check if player already exists
+				$sql="SELECT COUNT(Name) as UNIVIDEO FROM Users WHERE Name like '" . mysqli_real_escape_string($conn, $NameApp ) . "'";
+				$result=$conn->query($sql);
+				while($row = $result->fetch_assoc()){
+				$nameFound=$row['UNIVIDEO'];
+													}
+				//end of the chekerin
+				if($nameFound==0){ //if it's a new name then proceed with the registration
+				$passCreate=$val1 . $val2;
+				$passCreate=md5($passCreate);
+				$sql="INSERT INTO Users (Name,Password,Overall_Score,Win,Lost) VALUES ('" . mysql_real_escape_string($conn, $val1) . "' , '" . $passCreate . "',0,0,0)";
+				$result=$conn->query($sql);}
+				
+				$parameters = array("RegisterStatus" => $nameFound); // buildin the Response JSON     , 0 means the registration was a success , !=0 means it was not.			
+		}
+		
+		
 		
 	}
 	
-if($op == "SendPawnPosition") // example returning the position of a pawn.
-	{
-		$sql="SELECT * FROM Pawn WHERE ID_PLayer like '". $playerId . "' and Pawn_id like '". $pawnId . "'" ; 	
+	
+	if($op == "READ") {
+		if($tableName=="game"){//sending all pawn' positions
+			$parameters = array("gameId" => $gameId);
+			$sql="SELECT * FROM Pawn WHERE id_Player like " . $playerId . " AND id_Game like " . $gameId ;
+			$result=$conn->query($sql);
+				while($row = $result->fetch_assoc()){//Player1's Pawn
+				$parameters['playerId'][$playerId]['pawnId'][$row['ID']]['playerId'][]=$row['id_Player'];
+				$parameters['playerId'][$playerId]['pawnId'][$row['ID']]['position'][]=$row['Position'];
+													}	
+														
+				$sql="SELECT * FROM Pawn WHERE id_Player like " . $player2Id . " AND id_Game like " . $gameId ;
+				$result=$conn->query($sql);
+				while($row = $result->fetch_assoc()){//Player2's Pawn
+				$parameters['playerId'][$playerId]['pawnId'][$row['ID']]['playerId'][]=$row['id_Player'];
+				$parameters['playerId'][$playerId]['pawnId'][$row['ID']]['position'][]=$row['Position'];
+													}	
+													
+		}
+		
+		
+		if($tableName=="pawn" && $playerId!=NULL){ //if i do have the player id then i've understood the app wants the amount of pawns in the sent pawn $pos
+												//!!REMEMBER TO SEND $POS , IF NOT ,LET ME KNOW SINCE I HAVE TO DO ANOTHER QUERY (totally doable)
+			$sql="SELECT COUNT(ID) AS EJA FROM Pawn WHERE ID_Player like '". $playerId . "' and id_Game like '". $gameId . "' and Position like " .$pos ; 	
 		$result=$conn->query($sql);
 		while($row = $result->fetch_assoc()){
-		$pawnPos=$row['Position'];
+		$NumPawn=$row['EJA'];
 											}
-											
-		$parameters = array("pawnPos" => $pawnPos, "pawnId" => $pawnId , "playerId" => $playerId); // buildin the Response JSON
-
+		$parameters = array("gameId" => $gameId, "playerId" => $playerId , "position" => $NumPawn); // buildin the Response JSON
+					
+		}
+			
 		
-	}
-
-//if($op == "SendNumberOfPawns") elect count( Posizione) FROM Pawn WHERE posizione Like pos idplayer=idCurrentPlayer() && idgame=currentGame()
-//mi ritorna il numero di pedine impilate su una pedina del giocatore corrente	
-
-
-	if($op == "") 
-	{
-		$sql="" ; 
-		//...
 		
-		$parameters = array('1' => $..., ... );
 
 	}
 
