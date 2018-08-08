201bd4f7ea424b77781004fe05203768755e44d1
ShiroAimai <shiroaimai@gmail.com>
2018-08-08 16:30:26 +0200

    fix login.java

--- app/src/main/java/com/project/wirelessteam/wirelessproject/login.java
+++ app/src/main/java/com/project/wirelessteam/wirelessproject/login.java
@@ -16,63 +16,44 @@ import API.phpConnect;
 public class login extends AppCompatActivity {
 
  private boolean loginVar = false;
-    //private String Pass= ((TextView)findViewById(R.id.editText)).getText().toString();
-    //private String Name= ((TextView)findViewById(R.id.editText2)).getText().toString();
+    private String Pass= ((TextView)findViewById(R.id.editText4)).getText().toString();
+    private String Name= ((TextView)findViewById(R.id.editText)).getText().toString();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
 
-
-    class Controllo extends AsyncTask<String,Void,Boolean> {
-        private String Pass= ((TextView)findViewById(R.id.editText4)).getText().toString();
-        private String Name= ((TextView)findViewById(R.id.editText)).getText().toString();
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+    public void sendLogin() {
+        phpConnect conn = new phpConnect("https://psionofficial.com/Wireless/login.php",-1);
+        conn.execute("r",Name,"-1",makeMD5(Name+Pass),"-2");
+        if(conn.getResult() == true){
+            ((TextView) findViewById(R.id.textView2)).setText("LOGIN CORRETTO!");
+            loginVar = true;
+            goToSetup();
+        }
+        else {
+            ((TextView) findViewById(R.id.textView2)).setText("LOGIN ERRATO!");
+            loginVar = false;
         }
 
-        @Override
-        protected Boolean doInBackground(String... params) {
-            phpConnect conn = new phpConnect("https://psionofficial.com/Wireless/login.php",-1);
-            StringBuffer sb;
-            try{
+    }
+
+    private String makeMD5(String param) {
+        StringBuffer sb = null;
+        try{
             MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update((Name+Pass).getBytes());
+            md.update((param).getBytes());
             byte[] digest = md.digest();
             sb = new StringBuffer();
             for(byte b : digest)
                 sb.append(String.format("%02x", b & 0xff));
 
-            conn.execute("r",Name,"-1",sb.toString(),"-2");
-            }catch(NoSuchAlgorithmException e){
-                e.printStackTrace();
-            }
-
-            return conn.getResult();
-
+        }catch(NoSuchAlgorithmException e){
+            e.printStackTrace();
         }
-
-        @Override
-        protected void onPostExecute(Boolean r) {
-            if (r == true) {
-                ((TextView) findViewById(R.id.textView2)).setText("LOGIN CORRETTO!");
-                loginVar = true;
-                goToSetup();
-
-            } else {
-                ((TextView) findViewById(R.id.textView2)).setText("LOGIN ERRATO!");
-                loginVar = false;
-            }
-        }
+        return sb.toString();
     }
-    public void sendLogin(View v) {
-
-            new Controllo().execute();
-
-    }
-
     private void goToSetup() {
         if(loginVar == true) {
             loginVar = false;
