3195cef0b4ddeab651b0087a41b0c352bd16b6d4
ShiroAimai <shiroaimai@gmail.com>
2018-08-27 18:02:34 +0200

    added new drag&drop method

---
+++ app/src/main/java/Utils/onTouchCustomMethod.java
@@ -0,0 +1,30 @@
+package Utils;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.project.wirelessteam.wirelessproject.BoardActivity;
+
+public class onTouchCustomMethod implements View.OnTouchListener{
+    private int _xDelta;
+    private int _yDelta;
+    private RelativeLayout refLayout = null;
+    private Context boardContext = null;
+
+    public onTouchCustomMethod(Context context,RelativeLayout lay){
+        refLayout = lay;
+        boardContext = context;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        ClipData dragData = ClipData.newPlainText("",(String)view.getTag());
+        View.DragShadowBuilder shadow = new View.DragShadowBuilder(view);
+        view.startDrag(dragData,shadow,view,0);
+        return true;
+    }
+}
